62347cff53ba8fe5395138c889e9dcbfd8e7673b
Soumil Jaiswal <soumiljaiswal06@gmail.com>
2026-07-08 18:35:46 +0530

    Add files via upload

--- MySQL full series/11. Subqueries statements.sql
+++ MySQL full series/11. Subqueries statements.sql
@@ -17,4 +17,17 @@ select first_name , salary ,
 end as lower_or_higher
 from employee_salary;
 
+select gender , avg(age) , max(age) , min(age),  count(age) 
+from employee_demographics
+group by gender ; 
+
+select gender , avg(max_age)        
+from (select gender ,
+ avg(age) as avg_age  ,
+ max(age) as max_age  , 
+ min(age) as min_age , 
+ count(age) as count_age  
+from employee_demographics 
+group by gender ) as agg_table 
+group by gender ;
 
